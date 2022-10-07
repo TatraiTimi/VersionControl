98ad94a664425ddab8315d99045ca3f5dbcb12f2
TatraiTimi <timea.tatrai@stud.uni-corvinus.hu>
2022-10-07 16:53:40 +0200

    adatok exportálása1

--- Excel_aqefte/Excel_aqefte/Form1.cs
+++ Excel_aqefte/Excel_aqefte/Form1.cs
@@ -15,9 +15,9 @@ namespace Excel_aqefte
 {
     public partial class Form1 : Form
 
-     {
+    {
         List<Flat> flats;
-        RealEstateEntities context= new RealEstateEntities();
+        RealEstateEntities context = new RealEstateEntities();
 
         Excel.Application xlApp;
         Excel.Workbook xlwb;
@@ -37,8 +37,8 @@ namespace Excel_aqefte
                 xlApp = new Excel.Application();
                 xlwb = xlApp.Workbooks.Add(Missing.Value);
                 xlws = xlwb.ActiveSheet;
-                //CreateTable();
-                xlApp.Visible=true;
+                CreateTable();
+                xlApp.Visible = true;
                 xlApp.UserControl = true;
             }
             catch (Exception ex)
@@ -48,15 +48,66 @@ namespace Excel_aqefte
                 MessageBox.Show(errMsg, "Error");
                 xlwb.Close(false, Type.Missing, Type.Missing);
                 xlApp.Quit();
-                xlwb=null;
-                xlApp=null;
+                xlwb = null;
+                xlApp = null;
 
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < 8; i++)
+            {
+                xlws.Cells[1, 1] = headers[0];
+                xlws.Cells[1,2]=headers[1]; 
+                xlws.Cells[1,3]=headers[2]; 
+                xlws.Cells[1,4]=headers[3]; 
+                xlws.Cells[1,5]=headers[4]; 
+                xlws.Cells[1,6]=headers[5]; 
+                xlws.Cells[1,7]=headers[6]; 
+                xlws.Cells[1,8]=headers[7]; 
+                xlws.Cells[1,9]=headers[8]; 
+            }
+
+            object[,] values = new object[flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter,5]=f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = f.Price / f.FloorArea;
+                counter++;  
             }
         }
 
         private void LoadData()
         {
-            flats=context.Flats.ToList();
+            flats = context.Flats.ToList();
         }
     }
 }
