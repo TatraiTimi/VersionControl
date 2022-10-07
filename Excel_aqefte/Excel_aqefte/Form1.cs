b2f4c44994ceb1a95e603605b94cf6200745cbb2
TatraiTimi <timea.tatrai@stud.uni-corvinus.hu>
2022-10-07 16:23:39 +0200

    excel munkafüzet létrehozása és megnyitása

--- Excel_aqefte/Excel_aqefte/Form1.cs
+++ Excel_aqefte/Excel_aqefte/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace Excel_aqefte
 {
@@ -17,10 +18,40 @@ namespace Excel_aqefte
      {
         List<Flat> flats;
         RealEstateEntities context= new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlwb;
+        Excel.Worksheet xlws;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlwb = xlApp.Workbooks.Add(Missing.Value);
+                xlws = xlwb.ActiveSheet;
+                //CreateTable();
+                xlApp.Visible=true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error:{0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlwb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlwb=null;
+                xlApp=null;
+
+            }
         }
 
         private void LoadData()
