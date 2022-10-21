c6e96a3f39ce79e00410d2888d9a15ece40c8504
TatraiTimi <timea.tatrai@stud.uni-corvinus.hu>
2022-10-21 10:26:07 +0200

    webszolgáltatás hozzáadása és hívása

--- week05_aqefte/week05_aqefte/Form1.cs
+++ week05_aqefte/week05_aqefte/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05_aqefte.MnbServiceReference;
 
 namespace week05_aqefte
 {
@@ -15,6 +16,20 @@ namespace week05_aqefte
         public Form1()
         {
             InitializeComponent();
+            ExchangeRates();
+        }
+
+        private void ExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
