185a8fe39714fdb5ce7ec06eff78dcc5d6d41f79
TatraiTimi <timea.tatrai@stud.uni-corvinus.hu>
2022-11-16 18:49:40 +0100

    autó létrehozása

---
+++ aqefte_week06/aqefte_week06/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using aqefte_week06.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aqefte_week06.Entities
+{
+    internal class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imagefile = Image.FromFile("Images/car.png");
+            g.DrawImage(imagefile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
