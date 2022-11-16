8b97847e5df54686a0108e8ec9c5bd4912ccf95b
TatraiTimi <timea.tatrai@stud.uni-corvinus.hu>
2022-11-16 18:35:16 +0100

    absztrakció felhasználása a labdában

--- aqefte_week06/aqefte_week06/Entities/Ball.cs
+++ aqefte_week06/aqefte_week06/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aqefte_week06.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,30 +9,11 @@ using System.Windows.Forms;
 
 namespace aqefte_week06.Entities
 {
-    public class Ball:Label
+    public class Ball : Toy
     {
-        public Ball()
+        protected override void DrawImage(Graphics g)
         {
-            AutoSize = false;
-            Width = 50;
-            Height = 50;
-            Paint += Ball_Paint;
-
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics graphics)
-        {
-            graphics.FillEllipse(new SolidBrush(Color.Blue),0,0,Width,Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
     }
 }
